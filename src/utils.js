019dccdcb3a3e22f538d5c512abbb6b614152dfc
Elvira <eliasufa@yahoo.com>
2023-05-11 20:20:49 +0500

    homework1-react is ready

--- src/utils.js
+++ src/utils.js
@@ -7,22 +7,24 @@ const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
  * @param children {...Node} Вложенные элементы
  * @returns {HTMLElement}
  */
-export function createElement(name, props = {}, ...children) {
-  const element = document.createElement(name);
 
-  // Назначение свойств и атрибутов
-  for (const name of Object.keys(props)) {
-    if (propNames.has(name)) {
-      element[name] = props[name];
-    } else {
-      element.setAttribute(name, props[name]);
-    }
+export const enumLinkWord = (number) => {
+  const nums = [2, 0, 1, 1, 1, 2];
+  const words = ['раз', 'раза', 'раз'];
+  let finalWord;
+
+  if (number === 0) {
+    finalWord = titles[2];
+  } else if (number % 100 > 4 && number % 100 < 20) {
+    finalWord = words[2];
+  } else {
+    finalWord = words[nums[Math.min(number % 10, 5)]];
   }
 
-  // Вставка вложенных элементов
-  for (const child of children) {
-    element.append(child);
-  }
-
-  return element;
+  return finalWord;
 }
+
+/**
+ * Создание элемента со свойствами и вложенными элементами
+ * @returns {number}
+ */
