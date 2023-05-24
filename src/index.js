5aafab3075913d0469d1c6ba6508187221242532
Elvira <eliasufa@yahoo.com>
2023-05-24 18:02:45 +0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {generateCode} from './utils.js';
-import App from './app.js';
-import Store from './store.js';
-
-const store = new Store({
-  list: [
-    {code: generateCode(), title: 'Название товара', price: 100.0},
-    {code: generateCode(), title: 'Книга про React', price: 770},
-    {code: generateCode(), title: 'Конфета', price: 33},
-    {code: generateCode(), title: 'Трактор', price: 7955320},
-    {code: generateCode(), title: 'Телефон iPhone XIXV', price: 120000},
-    {code: generateCode(), title: 'Карандаши цветные', price: 111},
-    {code: generateCode(), title: 'Товар сюрприз', price: 0},
-  ],
-	cartList: [],
-	cartPriceTotal: 0,
-	cartCountTotal: 0
-});
-
-const root = createRoot(document.getElementById('root'));
-
-store.subscribe(() => {
-  root.render(<App store={store}/>);
-});
-
-// Первый рендер приложения
-root.render(<App store={store}/>);
+let username = "my name";
+let bonusBalance = 1000;
+let bonusPlus = 50;
+let bonusMinus = 3;
+let finalBonusCount = bonusBalance + (bonusPlus * 3) - (bonusMinus * 7);
+console.log(`Пользователь ${username}`);
+console.log(`Баланс ${bonusBalance}`);
+console.log(`Баланс на конец недели: ${finalBonusCount}`);
